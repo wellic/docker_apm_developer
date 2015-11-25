f1c88bb174024385c599e52ca5816e9b0ca698db
Valerii Savchenko <valerii.s@giantleaplab.com>
2015-11-25 15:52:47 +0200

    Added mysql #2

--- mysql/etc/phpmyadmin/config.inc.php
+++ mysql/etc/phpmyadmin/config.inc.php
@@ -2,6 +2,19 @@
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
+$cfg['Servers'][$i]['verbose'] = 'mysql 5.5';
+$cfg['Servers'][$i]['host'] = '127.0.0.1';
+$cfg['Servers'][$i]['port'] = '33055';
+$cfg['Servers'][$i]['connect_type'] = 'tcp';
+$cfg['Servers'][$i]['compress'] = false;
+/* Select mysql if your server does not have mysqli */
+$cfg['Servers'][$i]['extension'] = 'mysqli';
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
+$i++;
+
+/* Authentication type */
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
 $cfg['Servers'][$i]['verbose'] = 'mysql 5.6';
 $cfg['Servers'][$i]['host'] = '127.0.0.1';
 $cfg['Servers'][$i]['port'] = '33056';
